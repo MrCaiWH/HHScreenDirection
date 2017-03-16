8c0cded638c545c81259e239177616bc2d782442
蔡万鸿 <wanhong.cai@zerouav.com>
2017-03-16 13:35:30 +0800

    初始化

---
+++ HHScreenDirectionDemo/HHScreenDirectionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HHScreenDirectionDemo
+//
+//  Created by zero on 2017/3/16.
+//  Copyright © 2017年 Zerotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HHScreenDirectionDemo/HHScreenDirectionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HHScreenDirectionDemo
+//
+//  Created by zero on 2017/3/16.
+//  Copyright © 2017年 Zerotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
